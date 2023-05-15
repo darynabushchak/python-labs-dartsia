74c091e4db01a4af58a96f0131127b9629d1cd86
darynabushchak <dartsiabushchak@gmail.com>
2023-05-15 03:31:35 +0300

    added docs fot methods

--- chair.py
+++ chair.py
@@ -1,18 +1,113 @@
 class Chair:
-    def __init__(self, material, max_weight, owner=None, id=1):
-        self.material = material
-        self.max_weight = max_weight
-        self.owner = owner
-        self.id = id
+    """
+    The Chair class represents a chair object, which has attributes such as material, maximum weight capacity,
+    owner, and an identifier.
 
-    def occupy(self, owner):
-        self.owner = owner
+    Attributes:
+        __material (str): The material of the chair.
+        __max_weight (float): The maximum weight capacity of the chair.
+        __owner (str, optional): The current owner of the chair (default is None).
+        __id (int): The identifier for the chair (default is 1).
 
-    def release(self):
-        self.owner = None
+    Methods:
+        occupy(self, owner):
+            Occupies the chair by assigning an owner.
+            Args:
+                owner (str): The name of the owner.
 
-    def is_occupied(self):
-        return self.owner is not None
+        release(self):
+            Releases the chair by removing the current owner.
+
+        is_occupied(self):
+            Checks if the chair is currently occupied.
+            Returns:
+                bool: True if the chair is occupied, False otherwise.
+    """
+
+    instance = None
+
+    def __init__(self, __material=None, __max_weight=None, __owner=None, __id=1):
+        """
+        Initializes a new instance of the Chair class.
+
+        Args:
+            __material (str): The material of the chair.
+            __max_weight (float): The maximum weight capacity of the chair.
+            __owner (str, optional): The current owner of the chair (default is None).
+            __id (int, optional): The identifier for the chair (default is 1).
+        """
+        self.__material = __material
+        self.__max_weight = __max_weight
+        self.__owner = __owner
+        self.__id = __id
 
     def __str__(self):
-        return f"Chair ID: {self.id}\nMaterial: {self.material}\nMax weight: {self.max_weight}\nOwner: {self.owner}"
+        """
+        Returns a string representation of the chair.
+
+        Returns:
+            str: A formatted string containing the chair's ID, material, maximum weight, and owner (if any).
+        """
+        return f"Chair ID: {self.__id}\nMaterial: {self.__material}\nMax weight: {self.__max_weight}\nOwner: {self.__owner}"
+
+    @staticmethod
+    def get_instance():
+        if Chair.instance is None:
+            Chair.instance = Chair()
+        return Chair.instance
+
+    @property
+    def material(self):
+        return self.__material
+
+    @material.setter
+    def material(self, value):
+        self.__material = value
+
+    @property
+    def max_weight(self):
+        return self.__max_weight
+
+    @max_weight.setter
+    def max_weight(self, value):
+        self.__max_weight = value
+
+    @property
+    def owner(self):
+        return self.__owner
+
+    @owner.setter
+    def owner(self, value):
+        self.__owner = value
+
+    @property
+    def id(self):
+        return self.__id
+
+    @id.setter
+    def id(self, value):
+        self.__id = value
+
+    def occupy(self, owner):
+        """
+        Occupies the chair by assigning an owner.
+
+        Args:
+            owner (str): The name of the owner.
+        """
+        self.__owner = owner
+
+    def release(self):
+        """
+        Releases the chair by removing the current owner.
+        """
+        self.__owner = None
+
+    def is_occupied(self):
+        """
+        Checks if the chair is currently occupied.
+
+        Returns:
+            bool: True if the chair is occupied, False otherwise.
+        """
+        return self.__owner is not None
