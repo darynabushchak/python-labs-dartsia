87324c1841a5a6390b6c7c226417fba72299ee3d
darynabushchak <dartsiabushchak@gmail.com>
2023-05-22 01:24:01 +0300

    lost docstring

--- manager/chair_manager.py
+++ manager/chair_manager.py
@@ -1,4 +1,20 @@
 class ChairManager:
+    """
+       A class that manages a collection of chairs.
+
+       Attributes:
+           chairs (list): A list of chairs managed by the ChairManager.
+
+       Methods:
+           add_chair(chairs_to_add):
+               Adds a chair or a list of chairs to the collection.
+
+           find_all_by_material(material):
+               Finds all chairs in the collection with the specified material.
+
+           find_all_with_max_weight_heavier_than(max_weight):
+               Finds all chairs in the collection with a maximum weight heavier than the specified value.
+       """
     def __init__(self):
         self.chairs = []
 
